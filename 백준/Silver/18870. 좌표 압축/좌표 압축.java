71290e3470fe07d3a8240df8f0f6c378e95095b9
Hayoung Yu <55646863+ruhazle@users.noreply.github.com>
2024-01-11 11:33:39 +0900

    [Silver II] Title: 좌표 압축, Time: 1884 ms, Memory: 218544 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,16 +1,17 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int[] beforeCompression = new int[n];
         int[] afterCompression = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++) {
-            beforeCompression[i] = sc.nextInt();
+            beforeCompression[i] = Integer.parseInt(st.nextToken());
         }
 
         int[] sortedArr = Arrays.stream(beforeCompression).distinct().sorted().toArray();
