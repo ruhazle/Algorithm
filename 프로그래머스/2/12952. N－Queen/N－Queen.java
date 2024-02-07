1c64c76f79516bfe2022be8518e7e743c952bece
Hayoung Yu <55646863+ruhazle@users.noreply.github.com>
2024-02-07 11:12:19 +0900

    [level 2] Title: N-Queen, Time: 173.25 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,6 +1,31 @@
 class Solution {
+    static int[] board;
+    static int answer;
     public int solution(int n) {
-        int answer = 0;
+        answer = 0;
+        board = new int[n];
+        DFS(0, n);
         return answer;
+    }
+    
+    void DFS(int r, int n) {
+        if(r == n)
+            answer++;
+        else {
+            for(int i = 0; i < n; i++) {
+                board[r] = i;
+                if(check(r))
+                    DFS(r+1, n);
+            }
+        }
+    }
+    
+    boolean check(int r) {
+        for(int i = 0; i < r; i++) {
+            if(board[i] == board[r] 
+               || Math.abs(i - r) == Math.abs(board[i] - board[r]))
+               return false;
+        }
+        return true;
     }
 }
